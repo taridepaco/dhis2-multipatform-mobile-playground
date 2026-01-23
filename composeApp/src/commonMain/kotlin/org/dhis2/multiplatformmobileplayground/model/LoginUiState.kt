7f1d11f3dbb5000c725efef47a17b6fbae1bfe75
danielTari <daniel@dhis2.org>
2026-01-23 10:56:49 +0100

    add credentials as default labels

--- composeApp/src/commonMain/kotlin/org/dhis2/multiplatformmobileplayground/model/LoginUiState.kt
+++ composeApp/src/commonMain/kotlin/org/dhis2/multiplatformmobileplayground/model/LoginUiState.kt
@@ -3,9 +3,9 @@ package org.dhis2.multiplatformmobileplayground.model
 import androidx.compose.ui.text.input.TextFieldValue
 
 data class LoginUiState(
-    val serverUrl: TextFieldValue = TextFieldValue(""),
-    val username: TextFieldValue = TextFieldValue(""),
-    val password: TextFieldValue = TextFieldValue(""),
+    val serverUrl: TextFieldValue = TextFieldValue("https://play.im.dhis2.org/stable-2-42-3-1/"),
+    val username: TextFieldValue = TextFieldValue("android"),
+    val password: TextFieldValue = TextFieldValue("Android123"),
     val isLoading: Boolean = false,
     val errorMessage: String? = null,
     val isLoginSuccessful: Boolean = false
