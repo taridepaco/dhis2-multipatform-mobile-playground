1947a1b0be15c8fc52d03dcb01a72ea98d59ca46
andresmr <andres@andresmr.com>
2026-04-10 14:11:44 +0200

    Integrate Koin for dependency injection

--- composeApp/src/commonMain/kotlin/org/dhis2/multiplatformmobileplayground/App.kt
+++ composeApp/src/commonMain/kotlin/org/dhis2/multiplatformmobileplayground/App.kt
@@ -9,46 +9,41 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import org.dhis2.multiplatformmobileplayground.data.repository.RepositoryFactory
 import org.dhis2.multiplatformmobileplayground.ui.screens.HomeScreen
 import org.dhis2.multiplatformmobileplayground.ui.screens.LoginScreen
 import org.dhis2.multiplatformmobileplayground.viewmodel.HomeViewModel
 import org.dhis2.multiplatformmobileplayground.viewmodel.LoginViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
+import org.koin.compose.KoinContext
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
 fun App() {
-    MaterialTheme {
-        val loginViewModel: LoginViewModel = viewModel {
-            LoginViewModel(RepositoryFactory.createLoginRepository())
-        }
-        val loginUiState by loginViewModel.uiState.collectAsState()
+    KoinContext {
+        MaterialTheme {
+            val loginViewModel: LoginViewModel = koinViewModel()
+            val loginUiState by loginViewModel.uiState.collectAsState()
 
-        when {
-            loginUiState.isCheckingAuth -> {
-                Box(
-                    modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
+            when {
+                loginUiState.isCheckingAuth -> {
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        CircularProgressIndicator()
+                    }
                 }
-            }
-            loginUiState.isLoginSuccessful -> {
-                val homeViewModel: HomeViewModel = viewModel {
-                    HomeViewModel(
-                        RepositoryFactory.createUserRepository(),
-                        RepositoryFactory.createProgramRepository()
+                loginUiState.isLoginSuccessful -> {
+                    val homeViewModel: HomeViewModel = koinViewModel()
+                    HomeScreen(viewModel = homeViewModel)
+                }
+                else -> {
+                    LoginScreen(
+                        viewModel = loginViewModel,
+                        onLoginSuccess = { }
                     )
                 }
-                HomeScreen(viewModel = homeViewModel)
-            }
-            else -> {
-                LoginScreen(
-                    viewModel = loginViewModel,
-                    onLoginSuccess = { }
-                )
             }
         }
     }
